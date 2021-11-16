5f297e32f879e1e53bf83059be87e75c11aa7f92
Bradon Ladd <bradon.laddy0614@gmail.com>
2021-11-15 18:16:08 -0800

    Made visual changes to the website, deployed

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import logo from './logo.svg';
 import './App.css';
 import React from 'react';
+
 
 
 const Linkbox = () => {
@@ -12,7 +12,6 @@ const Linkbox = () => {
       <a href="https://www.linkedin.com/in/bradonladd/">
         <img className="links" id="linlogo" src={"images/linkedin.png"} width="50" height="50"alt="My Github"/>
       </a>
-      <img className="links" id="gmllogo" src={"images/gmail.png"} width="50" height="50"alt="My Github"/>
     </div>
   );
 }
@@ -22,9 +21,13 @@ const About = () => {
     <div id="aboutbox">
       <p id="abouttext">I'm a Computer Science student at Walla Walla University with experience in a wide variety of imperative and scripting languages, as well as familiarity 
           with common industry web frameworks. I’ve been programming since 2010 since an early introduction to, and 
-          fascination, with video game design. Later, in 2017, I placed more of my focus on industry software development. 
+          fascination, with video game design and coding. Later, in 2017, I shifted my focus on industry software development.
           I’m looking for a position where I can learn from team members and develop my proficiencies in a professional 
           environment, while drawing on previous experience from projects I’ve developed independently. Thanks for checking out the website!
+      </p>
+      <br />
+      <p id="abouttext">You can check out the repo for this website using the Github link above. As it is right now, all of my projects aren't hosted on there. I'm working on
+        porting my school and old personal projects to show off! Check back soon!
       </p>
     </div>
   )
